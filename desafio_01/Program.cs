922bc3d85f95c39515127cc390cbc43652c92334
Nelsontutia <xnelsontutia@yahoo.yahoo.com.br>
2025-08-22 09:18:39 -0300

    :tada: iniciando repo

---
+++ desafio_01/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i = 1; i <= 10; i++) // número base da tabuada
+{
+    Console.WriteLine($"Tabuada do {i}:");
+
+    for (int j = 1; j <= 10; j++) // multiplicador
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+
+} 
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
