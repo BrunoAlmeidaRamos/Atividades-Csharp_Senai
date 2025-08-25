ce3b9bcfe1e91a24bb01aebe3113737069686b47
Nelsontutia <xnelsontutia@yahoo.yahoo.com.br>
2025-08-25 11:59:10 -0300

    funcao

---
+++ funcao/funcao/Program.cs
@@ -0,0 +1,74 @@
+﻿//int[] numeros = new int[5];
+
+//int[] numero = { 10, 20, 30, 40, 50 };
+
+//Console.WriteLine($"{ numero[0]}");
+
+//numero[0] = 12;
+
+//Console.WriteLine($"{ numero[0]}");
+
+//for (int contador = 0; contador < numero.Length; contador++)
+//{
+//    Console.WriteLine($"posiçao {contador} = {numero[contador]}");
+//}
+
+
+//void Saudacao()
+//{
+//    Console.WriteLine("Qual e o seu nome? ");
+//    string nome = Console.ReadLine();
+//    Console.WriteLine($"Ola {nome}! Seja bem-vindo(a)!");
+//}
+
+//Saudacao();
+
+//int somar(int num1, int num2)
+//{
+//    Console.WriteLine("informe o primeiro numero :");
+//    int Num1 = Convert.ToInt32(num1);
+
+//    Console.WriteLine("informe o segundo numero: ");
+//    int Num2 = Convert.ToInt32(num2);
+
+//    int soma = num1 + num2;
+//    Console.WriteLine($"A SOMA DOS VALORES E {soma}");
+//    return soma;
+//}
+
+
+
+//using System.ComponentModel;
+
+//Console.WriteLine("============ Soma dos Elementos =====================");
+
+//int[] numeros = new int[5];
+//int soma = 0;
+
+//for (int i = 0; i < numeros.Length; i++)
+//{
+//    Console.Write($"Digite o número [{i + 1}] = ");
+//    numeros[i] = Convert.ToInt32(Console.ReadLine());
+//    soma += numeros[i]; // Soma diretamente enquanto lê
+//}
+
+//Console.WriteLine($"A soma dos elementos é: {soma}");
+
+    //int[] numeros = new int[10];
+
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    Console.Write($"Digite o {i + 1}º número: ");
+    //    numeros[i] = int.Parse(Console.ReadLine());
+    //}
+
+    //int maior = numeros[0];
+    //int menor = numeros[0];
+
+    //foreach (int num in numeros)
+    //{
+    //    if (num > maior) maior = num;
+    //    if (num < menor) menor = num;
+    //}
+
+    //Console.WriteLine($"Maior: {maior}, Menor: {menor}");
