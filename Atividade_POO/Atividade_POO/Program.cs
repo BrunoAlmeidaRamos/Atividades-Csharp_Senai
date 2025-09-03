8f29eecb802d6714dea02cb1a30b8ebefad5ceff
Nelsontutia <xnelsontutia@yahoo.yahoo.com.br>
2025-09-03 11:53:39 -0300

    Projeto em andameneto

---
+++ Atividade_POO/Atividade_POO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+
+Pessoa p = new Pessoa();
+p.nome = "bruno";
+p.idade = 22;
+p.Apresentar();
+
+public class Pessoa
+{
+    public string? nome;
+    public int idade;
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"ola, meu nome e {nome} e tenho {idade} anos.");
+    }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Nome: {nome}, Idade: {idade}");
+    }
+
+    public void AlterarIdade()
+    {
+        if (idade >= 0)
+        {
+            Console.Write("Digite sua idade: ");
+            int NovaIdade = Convert.ToInt32( Console.ReadLine() );
+        }
+        else
+        {
+            Console.WriteLine("Numero invalido! Tente novamente...");
+        }
+    }
+}
---
+++ Projeto_Estoque/Projeto_Estoque/Produto.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Projeto_Estoque;
+
+public class Produto
+{
+  
+}
